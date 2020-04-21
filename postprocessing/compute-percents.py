bca749c359c8bb7d3c721938d716d8a415effab4
Rebecca Morrison <rebeccam@colorado.edu>
2020-04-21 08:44:52 -0600

    update for computing quantiles

--- postprocessing/compute-percents.py
+++ postprocessing/compute-percents.py
@@ -2,7 +2,7 @@ import matplotlib
 import numpy as np
 from numpy import loadtxt
 
-burnin = 100;
+burnin = 2000;
 n_s = 7
 var = 25000000
 n_weeks = 52
@@ -35,8 +35,8 @@ for i in range(n_weeks*dim):
     nn = np.cumsum(n*(bins[1]-bins[0]))
     q0 = max((bins[np.argmax(nn>.5)] + bins[np.argmax(nn>.5) +1])/2,0)
     q1 = max((bins[np.argmax(nn>.025)] + bins[np.argmax(nn>.025) +1])/2,0)
-    q2 = max((bins[np.argmax(nn>.175)] + bins[np.argmax(nn>.25) +1])/2,0)
-    q3 = max((bins[np.argmax(nn>.825)] + bins[np.argmax(nn>.75) +1])/2,0)
+    q2 = max((bins[np.argmax(nn>.25)] + bins[np.argmax(nn>.25) +1])/2,0)
+    q3 = max((bins[np.argmax(nn>.75)] + bins[np.argmax(nn>.75) +1])/2,0)
     q4 = max((bins[np.argmax(nn>.975)] + bins[np.argmax(nn>.975) +1])/2,0)
     # file.write(str(np.mean(q[:,i]))+' '+str(np.percentile(q[:,i],2.5))+' '+str(np.percentile(q[:,i],25))+' '+str(np.percentile(q[:,i],75))+' '+str(np.percentile(q[:,i],97.5))+'\n')
     #file.write(str(np.mean(q[:,i]))+' '+str(q1)+' '+str(q2)+' '+str(q3)+' '+str(q4)+'\n')
